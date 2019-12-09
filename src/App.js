8fe085e947e34927b0a856e7044a1449dd2dc7a9
shaunmcarmody <shaun.carmody@live.co.uk>
2019-12-09 12:43:52 +0000

    add sortData method

--- src/App.js
+++ src/App.js
@@ -4,23 +4,27 @@ import './App.css';
 
 class App extends Component {
   state = {
-    contacts: []
+    contacts: [],
+    message: ''
   }
   componentDidMount() {
     axios
       .get('https://randomuser.me/api/?results=120')
-      .then(({ data }) => {
-        const contacts = data.results.sort((a, b) => {
-          const nameA = `${a.name.last}, ${a.name.first}`
-          const nameB = `${b.name.last}, ${b.name.first}`
-          return nameA.localeCompare(nameB)
-        })
-        this.setState({ contacts })
-      })
-      .catch(err => {
-        console.log(err)
+      .then(({ data }) => this.sortData(data))
+      .catch(({ message }) => {
+        this.setState({ message })
       });
   }
+
+  sortData(data) {
+    const contacts = data.results.sort((a, b) => {
+      const nameA = `${a.name.last}, ${a.name.first}`
+      const nameB = `${b.name.last}, ${b.name.first}`
+      return nameA.localeCompare(nameB)
+    })
+    this.setState({ contacts })
+  }
+
   render() {
     return (
       <div className="App">
