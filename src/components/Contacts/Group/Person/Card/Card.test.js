07ba3a4c79e7dbd828f5db8c0912deb7b13acf2e
shaunmcarmody <shaun.carmody@live.co.uk>
2019-12-10 15:58:29 +0000

    add unit tests to card component

--- src/components/Contacts/Group/Person/Card/Card.test.js
+++ src/components/Contacts/Group/Person/Card/Card.test.js
@@ -29,4 +29,39 @@ describe('<Card />', () => {
   it('renders successfully', () => {
     render(card);
   });
+
+  it('renders person name', () => {
+    const { getByText } = render(card)
+    getByText(/ALLEN, Molly/i)
+  })
+
+  it('renders person email', () => {
+    const { getByText } = render(card)
+    getByText(/molly.allen@example.com/i)
+  })
+
+  it('renders person phone number', () => {
+    const { getByText } = render(card)
+    getByText(/016974 44617/i)
+  })
+
+  it('renders person street', () => {
+    const { getByText } = render(card)
+    getByText(/5036 School Lane/i)
+  })
+
+  it('renders person city', () => {
+    const { getByText } = render(card)
+    getByText(/Chester/i)
+  })
+
+  it('renders person state', () => {
+    const { getByText } = render(card)
+    getByText(/Cheshire/i)
+  })
+
+  it('renders person state', () => {
+    const { getByText } = render(card)
+    getByText(/P7 7HD/i)
+  })
 });
