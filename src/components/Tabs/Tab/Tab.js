def0527ee42e41f7627c7825d685694bf875076d
shaunmcarmody <shaun.carmody@live.co.uk>
2019-12-29 12:04:34 +0000

    add replace p tag with span element

--- src/components/Tabs/Tab/Tab.js
+++ src/components/Tabs/Tab/Tab.js
@@ -6,26 +6,38 @@ const Button = styled.button`
   border: 1px solid grey;
   border-bottom: ${props => props.active ? '1px solid #f3f3f3' : '1px solid grey'}
   border-radius: 5px 5px 0 0;
-  cursor: pointer;
-  flex: 1;
-  padding-bottom: 5px;
   color: ${props => props.active ? 'black' : 'grey'}
+  cursor: pointer;
+  display: block;
   font-size: 2rem;
+  flex: 1;
+  padding: 0;
+  padding-bottom: 5px;
   text-align: center;
 
   @media(max-width: 700px) {
     border: 1px solid grey;
     border-right: ${props => props.active ? '1px solid #f3f3f3' : '1px solid grey'}
     border-radius: 0 5px 0 0;
+    font-size: 2rem;
+  }
+
+  &:focus {
+    outline: 0;
   }
 `
 
-const Entries = styled.p`
-  color: ${props => props.active ? 'black' : 'grey'}
+const SPAN = styled.span`
+  display: block;
   font-size: 1.2rem;
-  margin-right: 5%;
+  padding-right: 2px;
   text-align: right;
+
+  @media(max-width: 700px) {
+    font-size: 1.2rem;
+  }
 `
+
 
 const Tab = ({ activeTab, letter, entries, updateActiveGroup }) => {
   return (
@@ -34,11 +46,7 @@ const Tab = ({ activeTab, letter, entries, updateActiveGroup }) => {
       data-testid="tab-element"
       onClick={() => updateActiveGroup(letter)}
     >
-      <Entries
-        active={activeTab === letter}
-      >
-        {entries}
-      </Entries>
+      <SPAN>{entries}</SPAN>
       {letter}
     </Button>
   )
